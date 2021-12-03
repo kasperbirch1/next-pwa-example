06191ad85effb0be043b67f0c9474510426b4d7a
kbi <kbi@peytz.dk>
2021-12-03 13:07:53 +0100

    change pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+// const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
-    runtimeCaching,
+    disable: process.env.NODE_ENV === "development",
+    // dest: 'public', // comment out this line
+    register: true,
+    sw: "/sw.js",
   },
-})
+});
